d981020dcc471e13ed50d86fd7bebe7461e6f05f
unknown <moussahaad277@gmail.com>
2025-01-20 17:29:10 +0100

    Fix login toaster

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,17 +10,19 @@ export default async function LoginPage() {
     redirect("/");
   }
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-primary p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a
-          href="#"
-          className="flex items-center gap-2 self-center font-medium text-primary-foreground text-xl"
-        >
-          Dashboard GBEEB
-        </a>
-        <LoginForm />
+    <>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-primary p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium text-primary-foreground text-xl"
+          >
+            Dashboard GBEEB
+          </a>
+          <LoginForm />
+        </div>
       </div>
       <Toaster />
-    </div>
+    </>
   );
 }
